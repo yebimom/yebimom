bd2f526c50b2982680ab05c5cc7474c12344ce5d
dobestan <dobestan@gmail.com>
2015-02-28 12:29:08 +0900

    update settings : change default DB sqlite3 to postgresql

--- yebimom/settings.py
+++ yebimom/settings.py
@@ -64,8 +64,12 @@ WSGI_APPLICATION = 'yebimom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'yebimom',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
