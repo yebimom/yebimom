e2f04bd0106124815a4cfcb40910808456e018b2
dobestan <dobestan@gmail.com>
2015-03-01 18:01:46 +0900

    add comments : set staticfiles options for development, and production

--- yebimom/settings/heroku.py
+++ yebimom/settings/heroku.py
@@ -1,7 +1,17 @@
 from yebimom.settings.settings import *
 
 
+# partials/development.py
+
 DEBUG = False
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com", ".yebimom.com"]
+
+
+# partials/static.py
+
+# Serving static files from a cloud service or CDN
+# https://docs.djangoproject.com/en/1.7/howto/static-files/deployment/#serving-static-files-from-a-cloud-service-or-cdn
+STATIC_URL = '/static/'
+# STATIC_URL = 'https://cdn.yebimom.com/'
--- yebimom/settings/partials/static.py
+++ yebimom/settings/partials/static.py
@@ -1,4 +1,8 @@
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+
+# Serving the site and your static files from the same server
+# https://docs.djangoproject.com/en/1.7/howto/static-files/deployment/#serving-the-site-and-your-static-files-from-the-same-server
 STATIC_URL = '/static/'
+# STATIC_ROOT = 'some/path/to/static'
