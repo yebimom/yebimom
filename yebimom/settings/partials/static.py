7fe175fbbd5e179545f213dee8817491638112db
dobestan <dobestan@gmail.com>
2015-03-01 18:31:40 +0900

    update settings : set STATIC_ROOT to yebimom/static/ as default

--- yebimom/settings/partials/static.py
+++ yebimom/settings/partials/static.py
@@ -1,8 +1,11 @@
+import os
+from yebimom.settings.partials.application import BASE_DIR
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 
 # Serving the site and your static files from the same server
 # https://docs.djangoproject.com/en/1.7/howto/static-files/deployment/#serving-the-site-and-your-static-files-from-the-same-server
 STATIC_URL = '/static/'
-# STATIC_ROOT = 'some/path/to/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
