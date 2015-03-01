99d935dffc04aa61f77f2b5289f745be842e18b6
dobestan <dobestan@gmail.com>
2015-03-02 08:58:57 +0900

    update settings : components/ folder to store bower dependencies

--- yebimom/settings/partials/static.py
+++ yebimom/settings/partials/static.py
@@ -14,6 +14,8 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # This setting defines the additional locations the staticfiles app will
 # traverse if the FileSystemFinder finder is enabled
 # https://docs.djangoproject.com/en/1.7/ref/settings/#std:setting-STATICFILES_DIRS
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'components'),
 )
