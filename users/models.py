9c1be6ed5dff2665a222384c6274e53871424b76
corikachu <konomama0505@gmail.com>
2015-03-02 20:28:20 +0900

    update UserProfile : phone, birthday, is_male

--- users/models.py
+++ users/models.py
@@ -11,6 +11,9 @@ from django.db.models.signals import post_save
 class UserProfile(models.Model):
     user = models.OneToOneField(User, unique=True, primary_key=True)
     nickname = models.CharField(max_length=20, blank=True, null=True)
+    phone = models.CharField(max_length=11, blank=True, null=True)
+    birthday = models.DateField(blank=True, null=True)
+    is_male = models.BooleanField(default=True)
 
 
 def create_user_profile(sender, instance, created, **kwargs):
