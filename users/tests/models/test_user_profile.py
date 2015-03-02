52944da680d883fb2349751c70dcc94a2b0d55be
dobestan <dobestan@gmail.com>
2015-03-02 19:50:23 +0900

    add user_profile test : user profile should be created after user save

---
+++ users/tests/models/test_user_profile.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+
+# Django Models
+from django.contrib.auth.models import User
+from users.models import UserProfile
+
+# Django Exceptions
+from django.core.exceptions import ObjectDoesNotExist
+
+
+class UserProfileTest(TestCase):
+    def test_user_profile_should_created_after_user_save(self):
+        user = User.objects.create_user(username = "test_username")
+        try:
+            user.userprofile
+        except:
+            self.fail("User Profile should be created after User save")
