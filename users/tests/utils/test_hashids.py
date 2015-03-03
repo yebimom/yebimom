acfc63615ae90aa97630395512edfa120bafddf9
dobestan <dobestan@gmail.com>
2015-03-03 13:02:51 +0900

    update hashids tests : make userprofile_id random integer

--- users/tests/utils/test_hashids.py
+++ users/tests/utils/test_hashids.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from random import random
+
 from hashids import Hashids
 
 from users.utils import get_user_profile_hashids
@@ -10,6 +12,7 @@ from users.utils import decode_user_profile_hashids
 class HashidsTest(TestCase):
     def setUp(self):
         self.hashids = get_user_profile_hashids()
+        self.userprofile_id = int(random() * 1000)
 
     def test_get_user_profile_hashids_should_return_hashids_object(self):
         self.assertTrue(
@@ -18,13 +21,13 @@ class HashidsTest(TestCase):
 
     def test_encode_user_profile_hashids_should_return_encoded_value(self):
         self.assertEqual(
-            self.hashids.encode(1),
-            encode_user_profile_hashids(1)
+            self.hashids.encode(self.userprofile_id),
+            encode_user_profile_hashids(self.userprofile_id)
         )
 
     def test_docode_user_profile_hashids_should_return_decoded_value(self):
-        encoded_hashid = self.hashids.encode(1)
+        encoded_hashid = self.hashids.encode(self.userprofile_id)
         self.assertEqual(
-            1,
+            self.userprofile_id,
             decode_user_profile_hashids(encoded_hashid),
         )
