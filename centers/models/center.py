870c8ccae455070fef80484267f7cd2196e659bb
dobestan <dobestan@gmail.com>
2015-03-05 11:20:20 +0900

    update centers model : center should have region via ForeignKey, and RegionThirdLayer should return all centers list

--- centers/models/center.py
+++ centers/models/center.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+from centers.models.region import RegionThirdLayer
+
 
 class Center(models.Model):
+    region = models.ForeignKey(RegionThirdLayer)
+
     name = models.CharField(max_length=60)
     address = models.CharField(max_length=255, blank=True, null=True)
     phone = models.CharField(max_length=45, blank=True, null=True)
