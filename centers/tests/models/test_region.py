d1b9203304cabd196ce31262abc746cba3f562eb
dobestan <dobestan@gmail.com>
2015-03-05 11:18:23 +0900

    add test : RegionThirdLayer should return all centers list in region

---
+++ centers/tests/models/test_region.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Test
+from django.test import TestCase
+
+# Models
+from centers.models.region import RegionThirdLayer
+
+
+class RegionAllLayerTest(TestCase):
+    def test_region_third_layer_should_return_centers_list(self):
+        """
+        RegionThirdLayer 객체는
+        그 지역에 포함되어 있는 모든 산후조리원 리스트를 가져올 수 있어야 한다.
+        """
+
+        region_third_layer = RegionThirdLayer()
+        try:
+            region_third_layer.center_set.all()
+        except:
+            self.fail("RegionThirdLayer should have return all centers lists")
+
