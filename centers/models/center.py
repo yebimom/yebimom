5c4e7f5d2e5c24c256bf9ed6c1a302472902e05e
xme1226 <xme1226@gmail.com>
2015-03-06 17:44:44 +0900

    update centers model :  hash_id don't have to have default option

--- centers/models/center.py
+++ centers/models/center.py
@@ -14,7 +14,7 @@ class Center(models.Model):
     phone = models.CharField(max_length=45, blank=True, null=True)
     url = models.URLField(blank=True, null=True)
     hash_id = models.CharField(
-        max_length=12, unique=True, default="default")
+        max_length=12, unique=True)
 
     def __unicode__(self):
         return unicode(self.name)
