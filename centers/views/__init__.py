56d8f90f2e5f48fd0a56f613e6d0e8dca29cf163
wikibootup <wikibootup@gmail.com>
2015-03-07 02:17:02 +0900

    add view : upload center image view to centers app

---
+++ centers/views/__init__.py
@@ -0,0 +1,1 @@
+from upload_center_image import upload
---
+++ centers/views/upload_center_image.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from centers.forms.center_image_form import CenterImageForm
+from centers.utils.handle_uploaded_file import handle_uploaded_file
+
+
+def upload(request):
+    form = CenterImageForm(request.POST, request.FILES)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['image'])
+            return render(request, "home.html")
+
+    return render(request, "centers/upload.html", {'form': form})
