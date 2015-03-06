675f57543117a251d6813d39cb6176d3b9cb63c8
xme1226 <xme1226@gmail.com>
2015-03-06 15:44:34 +0900

    update centers model : have hash_id / when center created, center should update hash_id / add hashids methods

--- centers/models/center.py
+++ centers/models/center.py
@@ -1,4 +1,9 @@
 from django.db import models
+from hashids import Hashids
+from yebimom.settings.partials.application import HASHIDS_CENTER_SALT
+
+# Model Helper
+from django.db.models.signals import post_save
 
 
 class Center(models.Model):
@@ -8,6 +13,32 @@ class Center(models.Model):
     address = models.CharField(max_length=255, blank=True, null=True)
     phone = models.CharField(max_length=45, blank=True, null=True)
     url = models.URLField(blank=True, null=True)
+    hash_id = models.CharField(
+        max_length=12, unique=True, default="default")
 
     def __unicode__(self):
         return unicode(self.name)
+
+
+def update_center_hash_id(sender, instance, created, **kwargs):
+    if created:
+        # Update hash_id
+        Center.objects.filter(pk=instance.pk).update(
+            hash_id=encode_center_hashids(instance.id))
+
+post_save.connect(update_center_hash_id, sender=Center)
+
+
+# hashids methods
+def get_center_hashids():
+    return Hashids(salt=HASHIDS_CENTER_SALT, min_length=12)
+
+
+def encode_center_hashids(center_id):
+    hashids = get_center_hashids()
+    return hashids.encode(center_id)
+
+
+def decode_center_hashids(center_hash_id):
+    hashids = get_center_hashids()
+    return hashids.decode(center_hash_id)
