e69163127eabe4100e95df778603fc49c50c27ef
wikibootup <wikibootup@gmail.com>
2015-03-07 03:38:05 +0900

    refactor model : all models in centers app

--- centers/models/facility.py
+++ centers/models/facility.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class Facility(models.Model):
+    center = models.ForeignKey('Center')
+
     name = models.CharField(max_length=40)
     is_available = models.BooleanField(default=False)
-    center = models.ForeignKey('Center')
 
     def __unicode__(self):
         return unicode(self.name)
--- centers/models/program.py
+++ centers/models/program.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class Program(models.Model):
+    center = models.ForeignKey('Center')
+
     name = models.CharField(max_length=60)
     description = models.TextField(blank=True, null=True)
-    center = models.ForeignKey('Center')
 
     def __unicode__(self):
         return unicode(self.name)
