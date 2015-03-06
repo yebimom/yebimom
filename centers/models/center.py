6efbdcc3133c86fc939379266c56b2bedbd611fd
xme1226 <xme1226@gmail.com>
2015-03-07 00:18:59 +0900

    update center model : remove Hashids method in center model and change to using utils module

--- centers/models/center.py
+++ centers/models/center.py
@@ -1,9 +1,10 @@
 from django.db import models
-from hashids import Hashids
-from yebimom.settings.partials.application import HASHIDS_CENTER_SALT
 
 # Model Helper
 from django.db.models.signals import post_save
+
+# Util
+from centers.utils.center_hashids import get_encoded_center_hashid
 
 
 class Center(models.Model):
@@ -24,21 +25,6 @@ def update_center_hash_id(sender, instance, created, **kwargs):
     if created:
         # Update hash_id
         Center.objects.filter(pk=instance.pk).update(
-            hash_id=encode_center_hashids(instance.id))
+            hash_id=get_encoded_center_hashid(instance.id))
 
 post_save.connect(update_center_hash_id, sender=Center)
-
-
-# hashids methods
-def get_center_hashids():
-    return Hashids(salt=HASHIDS_CENTER_SALT, min_length=12)
-
-
-def encode_center_hashids(center_id):
-    hashids = get_center_hashids()
-    return hashids.encode(center_id)
-
-
-def decode_center_hashids(center_hash_id):
-    hashids = get_center_hashids()
-    return hashids.decode(center_hash_id)
