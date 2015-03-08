e4a945c60c70c0e0e59e58cc25d65672b108e53c
wikibootup <wikibootup@gmail.com>
2015-03-08 16:08:56 +0900

    refactor __init__ : explicit call of center models

--- centers/models/__init__.py
+++ centers/models/__init__.py
@@ -2,5 +2,5 @@ from center import Center
 from program import Program
 from facility import Facility
 from policy import Policy
-from region import *
-from image import *
+from region import RegionFirstLayer, RegionSecondLayer, RegionThirdLayer
+from image import CenterImage
