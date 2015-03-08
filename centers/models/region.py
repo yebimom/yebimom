1cd912c94000920d55d74af5046da11c87a356fb
dobestan <dobestan@gmail.com>
2015-03-08 15:37:42 +0900

    remove centers() comment : this should be called via center_set.all()

--- centers/models/region.py
+++ centers/models/region.py
@@ -10,20 +10,6 @@ class RegionFirstLayer(models.Model):
     def __unicode__(self):
         return unicode(self.name)
 
-    # def centers(self):
-    #     region_second_layer_set = self.regionsecondlayer_set.all()
-    #
-    #     region_third_layer_set = list()
-    #     for region_second_layer in region_second_layer_set:
-    #         region_third_layer_set.extend(
-    #             region_second_layer.regionthirdlayer_set.all()
-    #         )
-    #
-    #     centers = Center.objects.filter(
-    #         region__in=region_third_layer_set
-    #     )
-    #     return centers
-
 
 class RegionSecondLayer(models.Model):
     name = models.CharField(max_length=20)
@@ -34,13 +20,6 @@ class RegionSecondLayer(models.Model):
             self.first_layer.name,
             self.name
         )
-
-    # def centers(self):
-    #     region_third_layer_set = self.regionthirdlayer_set.all()
-    #     centers = Center.objects.filter(
-    #         region__in=region_third_layer_set
-    #     )
-    #     return centers
 
 
 class RegionThirdLayer(models.Model):
@@ -53,6 +32,3 @@ class RegionThirdLayer(models.Model):
             self.second_layer.name,
             self.name
         )
-
-    # def centers(self):
-    #     return self.center_set.all()
