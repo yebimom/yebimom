a5201e68af60ce485c1ce7f2460ee2af10c71ed5
wikibootup <wikibootup@gmail.com>
2015-03-08 16:15:55 +0900

    update settings : test media path

--- yebimom/settings/partials/media.py
+++ yebimom/settings/partials/media.py
@@ -1,4 +1,16 @@
-# Upload path
+import os
 
+
+# Upload path
 MEDIA_ROOT = ''
 MEDIA_URL = ''
+
+
+# Test Upload path
+TEST_MEDIA_DIR = 'TEST_MEDIA_DIR'
+
+TEST_MEDIA_ROOT = "%s/%s" % (
+    os.path.abspath(os.path.dirname(__file__)), TEST_MEDIA_DIR
+)
+
+TEST_MEDIA_URL = '/%s/' % TEST_MEDIA_DIR
