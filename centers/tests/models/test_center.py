39e064436462014a394b1fc9f29f8ca2047a33bd
dobestan <dobestan@gmail.com>
2015-03-08 15:10:55 +0900

    remove region helper test : should refactor using new region helpers, using foreignKey and pre_save signal

--- centers/tests/models/test_center.py
+++ centers/tests/models/test_center.py
@@ -1,21 +1,81 @@
+# -*- coding: utf-8 -*-
+
 from django.test import TestCase
 
 # Model
 from centers.models.center import Center
-
-# Test
-from centers.tests.models.test_region import RegionAllLayerTest
+from centers.models.region import RegionFirstLayer
+from centers.models.region import RegionSecondLayer
+from centers.models.region import RegionThirdLayer
 
 # Util
 from centers.utils.center_hashids import get_encoded_center_hashid
 from centers.utils.center_hashids import get_center_hashids_object
 
 
-class CenterTest(RegionAllLayerTest, TestCase):
+class CenterTest(TestCase):
 
     def setUp(self):
+        """
+        각각의 테스트를 수행하기 위해서 초기화 전략은 다음과 같다.
+
+        └── region_first_layer_0 ( RegionFirstLayer )
+            └── region_second_layer_0 ( RegionSecondLayer )
+                └── region_third_layer_0 ( RegionThirdLayer )
+                    └── center_0 ( Center )
+                    └── center_1 ( Center )
+
+                └── region_third_layer_1 ( RegionThirdLayer )
+                    └── center_2 ( Center )
+                    └── center_3 ( Center )
+
+            └── region_second_layer_1 ( RegionSecondLayer )
+
+        └── region_first_layer_1 ( RegionFirstLayer )
+        """
         self.hashids = get_center_hashids_object()
-        super(CenterTest, self).setUp()
+
+        # RegionFirstLayer
+        for region_first_layer in range(2):
+            RegionFirstLayer.objects.create(
+                name="RFL_%s" % region_first_layer
+            )
+
+        self.region_first_layer_0 = RegionFirstLayer.objects.first()
+        self.region_first_layer_1 = RegionFirstLayer.objects.last()
+
+        # RegionSecondLayer
+        for region_second_layer in range(2):
+            RegionSecondLayer.objects.create(
+                name="RSL_%s" % region_second_layer,
+                first_layer=self.region_first_layer_0
+            )
+
+        self.region_second_layer_0 = RegionSecondLayer.objects.first()
+        self.region_second_layer_1 = RegionSecondLayer.objects.last()
+
+        # RegionThirdLayer
+        for region_third_layer in range(2):
+            RegionThirdLayer.objects.create(
+                name="RTL_%s" % region_third_layer,
+                second_layer=self.region_second_layer_0
+            )
+
+        self.region_third_layer_0 = RegionThirdLayer.objects.first()
+        self.region_third_layer_1 = RegionThirdLayer.objects.last()
+
+        # Center
+        for center in range(2):
+            Center.objects.create(
+                name="center_%s" % center,
+                region=self.region_third_layer_0
+            )
+
+        for center in range(2, 4):
+            Center.objects.create(
+                name="center_%s" % center,
+                region=self.region_third_layer_1
+            )
 
     def test_encoded_center_hashids_should_have_valid_value(self):
         center = Center.objects.create(
