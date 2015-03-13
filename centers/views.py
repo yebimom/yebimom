6dcd0af43e33d3bd1fee855dc865893ce628b8e0
xme1226 <xme1226@gmail.com>
2015-03-13 13:56:11 +0900

    add views : if several values is submitted, they appear as result

--- centers/views.py
+++ centers/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def center_registration(request):
+    if request.method == 'GET':
+        return render(request, 'centers/center_registration.html')
+    else:
+        errors = []
+        try:
+            name = request.POST.get('name', 0)
+            address = request.POST.get('address', 0)
+            phone = request.POST.get('phone', 0)
+            url = request.POST.get('url', 0)
+            price = request.POST.get('price', 0)
+            result = name, address, phone, url, price
+        except ValueError:
+            errors.append('Not a Valid Value')
+            result = 0
+
+        return render(
+            request,
+            'centers/center_registration.html',
+            {'result': result, 'errors': errors}
+        )
+
+
+def center(request):
+    return HttpResponse("Center")
