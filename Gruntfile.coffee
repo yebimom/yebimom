d4ea936096bbdb34bef0574cedefae4adae01f9f
dobestan <dobestan@gmail.com>
2015-03-13 21:59:12 +0900

    refactor : Gruntfile.js to Gruntfile.coffee

---
+++ Gruntfile.coffee
@@ -0,0 +1,3 @@
+module.exports = (grunt) ->
+    grunt.registerTask 'default', () ->
+        grunt.log.writeln 'hello world'
--- Gruntfile.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(grunt){
-    grunt.registerTask("default", function(){
-        grunt.log.writeln("hello world");
-    })
-}
