5432271c372771317d37b731f1cbac658835573e
dobestan <dobestan@gmail.com>
2015-03-15 00:26:43 +0900

    add feature : basic signup feature

--- yebimom/urls.py
+++ yebimom/urls.py
@@ -16,4 +16,5 @@ urlpatterns = patterns(
     # Login, Logout
     url(r'^login/$', 'users.views.login_view', name='login'),
     url(r'^logout/$', 'users.views.logout_view', name='logout'),
+    url(r'^signup/$', 'users.views.signup', name='signup'),
 )
