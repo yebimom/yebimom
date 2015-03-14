c2885f01b9905287256b841e9dc1a9f0bce89b6f
dobestan <dobestan@gmail.com>
2015-03-15 02:27:14 +0900

    add static rule pages : including "service", "privacy", "disclaimer", "search_policy"

--- yebimom/urls.py
+++ yebimom/urls.py
@@ -17,4 +17,10 @@ urlpatterns = patterns(
     url(r'^login/$', 'users.views.login_view', name='login'),
     url(r'^logout/$', 'users.views.logout_view', name='logout'),
     url(r'^signup/$', 'users.views.signup', name='signup'),
+
+    # Rules ( Static Pages )
+    url(r'^rules/service/$', 'yebimom.views.service', name='service'),
+    url(r'^rules/privacy/$', 'yebimom.views.privacy', name='privacy'),
+    url(r'^rules/disclaimer/$', 'yebimom.views.disclaimer', name='disclaimer'),
+    url(r'^rules/search_policy/$', 'yebimom.views.search_policy', name='search_policy'),
 )
