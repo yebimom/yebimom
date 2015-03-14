ef2a3b5a5e02f317029d48511747b2011c848149
dobestan <dobestan@gmail.com>
2015-03-15 01:18:59 +0900

    add package "django-debug-toolbar" : https://github.com/django-debug-toolbar/django-debug-toolbar/

--- yebimom/settings/development.py
+++ yebimom/settings/development.py
@@ -5,6 +5,7 @@ INSTALLED_APPS += (
     # Django 3rd Party Modules ( installed via pip )
     'django_nose',
     'django_extensions',
+    'debug_toolbar',
 )
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
