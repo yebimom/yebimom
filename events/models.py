da370c85001040dc2e405b38700b5f1de606eb8f
wikibootup <wikibootup@gmail.com>
2015-03-15 06:20:54 +0900

    update model : event model fields('name', 'template_contents', 'is_in_progress')

--- events/models.py
+++ events/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=128)
+    template_contents = models.TextField(null=True)
+    is_in_progress = models.BooleanField(default=True)
