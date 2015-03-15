2e37d03418336145e5a7bc0aa412ee1607ebdfde
dobestan <dobestan@gmail.com>
2015-03-15 15:50:47 +0900

    add EventListView : show all events, should connect with detail page later

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 
 # Views
+from events.views import EventList
 from events.views import EventDetail
 
 
@@ -8,5 +9,6 @@ urlpatterns = patterns(
     '',
 
     # Event Urls
+    url(r'^events/$', EventList.as_view(), name='list'),
     url(r'^event/(?P<pk>\d+)/$', EventDetail.as_view(), name='detail'),
 )
--- events/views.py
+++ events/views.py
@@ -1,5 +1,15 @@
+# Views
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+# Models
 from events.models import Event
+
+
+class EventList(ListView):
+    model = Event
+    template_name = 'events/list.html'
+    context_object_name = 'events'
 
 
 class EventDetail(DetailView):
