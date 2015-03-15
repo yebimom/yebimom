c29dd23952659b8856d268a0487399ec1b043626
dobestan <dobestan@gmail.com>
2015-03-15 18:19:31 +0900

    add additional fields to api : property, and image url is available

--- events/serializers.py
+++ events/serializers.py
@@ -7,4 +7,4 @@ from events.models import Event
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ('id', 'title', 'contents', 'starts_at', 'ends_at')
+        fields = ('id', 'title', 'contents', 'starts_at', 'ends_at', 'is_in_progress', 'thumbnail')
