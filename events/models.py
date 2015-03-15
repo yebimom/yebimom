be6794fce377d1e884f24fad618050c00ec958bb
dobestan <dobestan@gmail.com>
2015-03-15 16:03:16 +0900

    update events model : starts_at, ends_at

--- events/models.py
+++ events/models.py
@@ -5,5 +5,8 @@ class Event(models.Model):
     title = models.CharField(max_length=255)
     contents = models.TextField(blank=True, null=True)
 
+    starts_at = models.DateTimeField(blank=True, null=True)
+    ends_at = models.DateTimeField(blank=True, null=True)
+
     def __unicode__(self):
         return self.title
