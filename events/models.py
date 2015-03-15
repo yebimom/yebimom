5362cc5109df20e840689d29a40f609432b6243d
wikibootup <wikibootup@gmail.com>
2015-03-15 13:36:49 +0900

    remove model field : event name field

--- events/models.py
+++ events/models.py
@@ -2,6 +2,5 @@ from django.db import models
 
 
 class Event(models.Model):
-    name = models.CharField(max_length=128)
     template_contents = models.TextField(null=True)
     is_in_progress = models.BooleanField(default=True)
