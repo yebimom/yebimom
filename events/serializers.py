0be04d8042093e23f4e19cee56f42b68e7f84794
dobestan <dobestan@gmail.com>
2015-03-15 18:34:02 +0900

    update Event API : using HyperlinkedIdentityField

--- events/serializers.py
+++ events/serializers.py
@@ -5,6 +5,9 @@ from events.models import Event
 
 
 class EventSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api:detail')
+    homepage = serializers.HyperlinkedIdentityField(view_name='events:detail')
+
     class Meta:
         model = Event
-        fields = ('id', 'title', 'contents', 'starts_at', 'ends_at', 'is_in_progress', 'thumbnail')
+        fields = ('id', 'title', 'contents', 'starts_at', 'ends_at', 'is_in_progress', 'thumbnail', 'url', 'homepage')
