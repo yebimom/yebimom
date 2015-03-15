1cdac25beb5cd4131ac3a481ef7161330d1c871f
dobestan <dobestan@gmail.com>
2015-03-15 16:54:35 +0900

    init django admin page for Event Model

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from events.models import Event
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    pass
