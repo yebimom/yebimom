f8effa28a8e6977182a416b9f61ba4659ea13513
dobestan <dobestan@gmail.com>
2015-03-16 18:43:39 +0900

    add very simple sitemap.xml using django.contrib.sitemaps

--- yebimom/urls.py
+++ yebimom/urls.py
@@ -4,11 +4,27 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.sitemaps import FlatPageSitemap, GenericSitemap
+from django.contrib.sitemaps.views import sitemap
+
+from events.models import Event
+
+
+info_dict = {
+    'queryset': Event.objects.all(),
+    'date_field': 'starts_at',
+}
+
+sitemaps = {
+    'event': GenericSitemap(info_dict, priority=0.6),
+}
+
 urlpatterns = patterns(
     '',
 
     # Django Default
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 
     # Django 3rd Party Modules
     url('', include('social.apps.django_app.urls', namespace='social')),
