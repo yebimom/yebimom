845babc72b100c76bedb50ca05e5f085b50a3898
xme1226 <xme1226@gmail.com>
2015-03-17 02:23:05 +0900

    update forms : change form name, import ugettext_lazy

--- centers/forms.py
+++ centers/forms.py
@@ -1,13 +1,16 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 # Model
 from centers.models.center import Center
 
 
-class Center(forms.ModelForm):
+class CenterForm(forms.ModelForm):
     class Meta:
         model = Center
-        fields = ['region_third_layer', 'name', 'address', 'phone', 'url', 'price']
+        fields = '__all__'
+        # fields = ['region_third_layer', 'name', 'address', 'phone', 'url', 'price']
+        # exclude = ('region_first_layer', 'region_second_layer')
         labels = {
             'region_third_layer': _('Region'),
             'name': _('Center Name'),
