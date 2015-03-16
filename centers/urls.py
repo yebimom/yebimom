a59a9254ef2da7ea982c27d8cdffb49fcad1c967
xme1226 <xme1226@gmail.com>
2015-03-17 03:37:51 +0900

    update urls : add names, complete url pattern

--- centers/urls.py
+++ centers/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns(
     url(r'^center/$', views.center),
 
     # Center registration
-    url(r'^center/register/', views.center_registration),
+    url(r'^center/register/', views.center_register, name='register'),
+    url(r'^center/register_complete/', views.center_register_complete, name='register_complete'),
 )
