93568d261a2487a2cd33d3fe9d7abc1beb722ecb
xme1226 <xme1226@gmail.com>
2015-03-17 02:25:13 +0900

    update views : change to creating CenterForm, sending center_form as parameter

--- centers/views.py
+++ centers/views.py
@@ -1,26 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
+# Form
+from centers.forms import CenterForm
 
 
 def center_registration(request):
     if request.method == 'GET':
-        return render(request, 'centers/center_registration.html')
+        center_form = CenterForm()
     else:
-        try:
-            name = request.POST.get('name', 0)
-            address = request.POST.get('address', 0)
-            phone = request.POST.get('phone', 0)
-            url = request.POST.get('url', 0)
-            price = request.POST.get('price', 0)
-            result = name, address, phone, url, price
-        except ValueError:
-            result = 0
+        center_form = CenterForm(request.POST)
 
-        return render(
-            request,
-            'centers/center_registration.html',
-            {'result': result}
-        )
+        if center_form.is_valid():
+            return HttpResponseRedirect('/center/register_ok')
+
+    return render(
+        request,
+        'centers/center_registration.html',
+        {'center_form': center_form}
+    )
 
 
 def center(request):
