9386f3ba30782001ac9540941dd9d3395ea16a40
xme1226 <xme1226@gmail.com>
2015-03-17 03:37:16 +0900

    update views : if form values are valid, save them as object. and adding complete view.

--- centers/views.py
+++ centers/views.py
@@ -5,21 +5,26 @@ from django.http import HttpResponse, HttpResponseRedirect
 from centers.forms import CenterForm
 
 
-def center_registration(request):
+def center_register(request):
     if request.method == 'GET':
         center_form = CenterForm()
     else:
         center_form = CenterForm(request.POST)
 
         if center_form.is_valid():
-            return HttpResponseRedirect('/center/register_ok')
+            center_form.save()
+            return HttpResponseRedirect('/center/register_complete')
 
     return render(
         request,
-        'centers/center_registration.html',
+        'centers/center_register.html',
         {'center_form': center_form}
     )
 
 
 def center(request):
     return HttpResponse("Center")
+
+
+def center_register_complete(request):
+    return HttpResponse("Center Registration COMPLETE!")
