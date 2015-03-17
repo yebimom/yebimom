3f5267213784dc42908d33f9b78c9ffd1a3e8d55
xme1226 <xme1226@gmail.com>
2015-03-18 01:34:39 +0900

    update urls : remove center patterns

--- centers/urls.py
+++ centers/urls.py
@@ -4,9 +4,9 @@ from centers import views
 urlpatterns = patterns(
     '',
 
-    url(r'^center/$', views.center),
+    url(r'^$', views.center),
 
     # Center registration
-    url(r'^center/register/', views.center_register, name='register'),
-    url(r'^center/register_complete/', views.center_register_complete, name='register_complete'),
+    url(r'^register/', views.center_register, name='register'),
+    url(r'^register_complete/', views.center_register_complete, name='register_complete'),
 )
