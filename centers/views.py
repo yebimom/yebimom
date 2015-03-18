eaf1e078f141286c23ca131d9a859713380d61c1
xme1226 <xme1226@gmail.com>
2015-03-18 20:35:32 +0900

    update views : add center view. if searched, it display centers list that contains query in their name

--- centers/views.py
+++ centers/views.py
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
 # Form
 from centers.forms import CenterForm
+
+# Model
+from centers.models.center import Center
 
 
 def center_register(request):
@@ -24,7 +28,14 @@ def center_register(request):
 
 
 def center(request):
-    return HttpResponse("Center")
+    if 'query' in request.GET and request.GET['query']:
+        query = request.GET['query']
+        centers = Center.objects.filter(name__icontains=query)
+        return render(request, 'centers/center_list.html',
+                      {'centers': centers, 'query': query}
+                      )
+    else:
+        return HttpResponse("검색을 클릭해주세요.")
 
 
 def center_register_complete(request):
