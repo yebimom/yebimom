e70c14b4543fda548f5fe8f9925cc479fd5e4ebf
wikibootup <wikibootup@gmail.com>
2015-03-18 21:37:40 +0900

    fix conflict : centers admin

--- centers/admin.py
+++ centers/admin.py
@@ -3,5 +3,13 @@ from django.contrib import admin
 # Model
 from centers.models.center import Center
 
+# Models
+from centers.models import CenterImage
+
 
 admin.site.register(Center)
+
+
+@admin.register(CenterImage)
+class CenterImageAdmin(admin.ModelAdmin):
+    pass
