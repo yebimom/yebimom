0aac9e6826f757ee94635a653bf1cb237824a71d
xme1226 <xme1226@gmail.com>
2015-03-18 22:25:06 +0900

    update urls : add center detail view url, it takes 5string(hash_id)

--- centers/urls.py
+++ centers/urls.py
@@ -9,4 +9,6 @@ urlpatterns = patterns(
     # Center registration
     url(r'^register/', views.center_register, name='register'),
     url(r'^register_complete/', views.center_register_complete, name='register_complete'),
+    # url(r'^detail/', views.center_detail, name='detail'),
+    url(r'^(?P<hash_id>\w{5})/$', views.center_detail, name='detail'),
 )
