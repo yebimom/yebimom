2f79d16d44a0c4c1275d30d9480d7f4179ce6654
corikachu <konomama0505@gmail.com>
2015-03-18 22:48:05 +0900

    fix foreignkey

--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from users.models.user import User
+from centers.models.center import Center
 
 
 class Review(models.Model):
@@ -6,5 +8,5 @@ class Review(models.Model):
     class Meta:
         unique_together = (('user_id', 'center_id'),)
 
-    user_id = models.ForeignKey('User')
-    center_id = models.ForeignKey('Center')
+    user_id = models.ForeignKey(User)
+    center_id = models.ForeignKey(Center)
