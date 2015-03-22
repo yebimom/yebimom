8a69ce838fffe11b87dd4a634bf029b8155d5d48
dobestan <dobestan@gmail.com>
2015-03-22 16:51:51 +0900

    move analytics tracking id to os.environment

--- yebimom/context_processors.py
+++ yebimom/context_processors.py
@@ -5,7 +5,7 @@ def google_analytics(request):
     """
     render Google Analytics tracking ID ( UA-********-* ) to template
     """
-    GOOGLE_ANALYTICS_TRACKING_ID = 'UA-60220587-1'
+    GOOGLE_ANALYTICS_TRACKING_ID = getattr(settings, 'GOOGLE_ANALYTICS_TRACKING_ID', False)
     return {
             'GOOGLE_ANALYTICS_TRACKING_ID': GOOGLE_ANALYTICS_TRACKING_ID,
     }
