4f68023ddbb8ea1a33606b79543a0f12417d0284
dobestan <dobestan@gmail.com>
2015-03-22 16:56:38 +0900

    add conditions : only render google-analytics tracking id, DEBUG is False, and tracking id exists.

--- yebimom/context_processors.py
+++ yebimom/context_processors.py
@@ -6,6 +6,8 @@ def google_analytics(request):
     render Google Analytics tracking ID ( UA-********-* ) to template
     """
     GOOGLE_ANALYTICS_TRACKING_ID = getattr(settings, 'GOOGLE_ANALYTICS_TRACKING_ID', False)
-    return {
-            'GOOGLE_ANALYTICS_TRACKING_ID': GOOGLE_ANALYTICS_TRACKING_ID,
-    }
+    if not settings.DEBUG and GOOGLE_ANALYTICS_TRACKING_ID:
+        return {
+                'GOOGLE_ANALYTICS_TRACKING_ID': GOOGLE_ANALYTICS_TRACKING_ID,
+        }
+    return {}
