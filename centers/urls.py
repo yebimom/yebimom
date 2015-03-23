b79f9d1059f859614cbab91503b4b206ce16a3eb
dobestan <dobestan@gmail.com>
2015-03-23 23:56:07 +0900

    remove center registration features

--- centers/urls.py
+++ centers/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, url
-from centers import views
 
 from centers.views import CenterList
 from centers.views import CenterDetail
@@ -9,8 +8,4 @@ urlpatterns = patterns(
 
     url(r'^$', CenterList.as_view(), name='list'),
     url(r'^(?P<slug>\w{5})/$', CenterDetail.as_view(), name='detail'),
-
-    # Center registration
-    url(r'^register/', views.center_register, name='register'),
-    url(r'^register_complete/', views.center_register_complete, name='register_complete'),
 )
