225297c38415fb4cafbaa2f78100cc2e46a72dfc
dobestan <dobestan@gmail.com>
2015-03-26 17:36:08 +0900

    update Facility, Policy model : remove is_available field, ManyToMany Field does not need this field

--- centers/models/policy.py
+++ centers/models/policy.py
@@ -6,7 +6,6 @@ class Policy(models.Model):
     center = models.ManyToManyField(Center)
 
     name = models.CharField(max_length=40)
-    is_available = models.BooleanField(default=False)
 
     def __unicode__(self):
         return unicode(self.name)
