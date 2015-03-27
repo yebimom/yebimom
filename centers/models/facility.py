a9deb6d5bf416b11acc02e43989bb6ef1a83a5f4
dobestan <dobestan@gmail.com>
2015-03-27 20:17:28 +0900

    update Facility Model : name is unique

--- centers/models/facility.py
+++ centers/models/facility.py
@@ -5,7 +5,7 @@ from centers.models.center import Center
 class Facility(models.Model):
     center = models.ManyToManyField(Center, blank=True)
 
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, unique=True)
 
     def __unicode__(self):
         return unicode(self.name)
