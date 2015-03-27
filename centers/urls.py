580bb0cec78f0098d5a55c561a45d2b721f1e8f4
dobestan <dobestan@gmail.com>
2015-03-27 22:20:41 +0900

    fix url names for creating reviews

--- centers/urls.py
+++ centers/urls.py
@@ -9,5 +9,8 @@ urlpatterns = patterns(
 
     url(r'^$', CenterList.as_view(), name='list'),
     url(r'^(?P<slug>\w{5})/$', CenterDetail.as_view(), name='detail'),
-    url(r'^(?P<slug>\w{5})/reviews/$', ReviewCreate.as_view(), name='reviews'),
+
+    url(r'^(?P<slug>\w{5})/review/$', ReviewCreate.as_view(), name='create_review'),
+    # url(r'^(?P<slug>\w{5})/review/edit/$', ReviewUpdate.as_view(), name='update_review'),
+    # url(r'^(?P<slug>\w{5})/review/delete/$', ReviewDelete.as_view(), name='delete_review'),
 )
