60ee5fc97ac00d3b3da478b34e99713ab47f7d0c
dobestan <dobestan@gmail.com>
2015-03-29 21:16:33 +0900

    init install.sh : install dependencies on Ubuntu

---
+++ install.sh
@@ -0,0 +1,6 @@
+# Update Package Manager ( apk ), and upgrade pre-installed pacakges
+apt-get update -y
+apt-get upgrade -y
+
+# Install Essential Packages
+apt-get install -y build-essential git python python-dev
