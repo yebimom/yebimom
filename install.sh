276ec22f741f050acb4d2fd42284fe827f9c7b46
dobestan <dobestan@gmail.com>
2015-03-29 21:27:00 +0900

    install pyenv, pyenv-virtualenv via https://github.com/yyuu/pyenv-installer

--- install.sh
+++ install.sh
@@ -4,3 +4,12 @@ apt-get upgrade -y
 
 # Install Essential Packages
 apt-get install -y build-essential git python python-dev
+
+# Install Python Version Manager ( pyenv ), Python Virtual Enviroment Manager ( virtualenv )
+# https://github.com/yyuu/pyenv-installer
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+source ~/.bashrc
