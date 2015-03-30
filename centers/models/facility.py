2d9b4cb6ddd2801a658f70fde9c09575c04a4dfc
wikibootup <wikibootup@gmail.com>
2015-03-30 16:35:12 +0900

    update model : update plural name for admin label

--- centers/models/facility.py
+++ centers/models/facility.py
@@ -3,8 +3,11 @@ from centers.models.center import Center
 
 
 class Facility(models.Model):
-    center = models.ManyToManyField(Center, blank=True)
 
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+    center = models.ManyToManyField(Center, blank=True)
     name = models.CharField(max_length=40, unique=True)
 
     def __unicode__(self):
