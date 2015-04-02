f29ff0717c9ba3ddceec2a85f9dd6f1d1b8b479e
dobestan <dobestan@gmail.com>
2015-04-02 22:58:54 +0900

    fix url : create review with type, in this case "visit"

--- centers/urls.py
+++ centers/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns(
     url(r'^$', CenterList.as_view(), name='list'),
     url(r'^(?P<slug>\w{5})/$', CenterDetail.as_view(), name='detail'),
 
-    url(r'^(?P<slug>\w{5})/review/$', VisitReviewCreate.as_view(), name='create_review'),
-    url(r'^(?P<slug>\w{5})/review/update/$', VisitReviewUpdate.as_view(), name='update_review'),
-    url(r'^(?P<slug>\w{5})/review/delete/$', VisitReviewDelete.as_view(), name='delete_review'),
+    url(r'^(?P<slug>\w{5})/review/visit/$', VisitReviewCreate.as_view(), name='create_visit_review'),
+    url(r'^(?P<slug>\w{5})/review/visit/update/$', VisitReviewUpdate.as_view(), name='update_visit_review'),
+    url(r'^(?P<slug>\w{5})/review/visit/delete/$', VisitReviewDelete.as_view(), name='delete_visit_review'),
 )
