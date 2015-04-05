5e24d50536fee77fcfb61236152e53cb5fb78d0a
wikibootup <wikibootup@gmail.com>
2015-04-05 14:33:50 +0900

    update contact model : send mail when created answer

--- yebimom/tasks.py
+++ yebimom/tasks.py
@@ -48,3 +48,44 @@ def send_question_email_to_user(email, content):
 def send_question_email(email, phone, title, content):
     send_question_email_to_user(email, content)
     send_question_email_to_admin(email, phone, title, content)
+
+
+def send_answer_email_to_admin(email, phone, question_title, content):
+    email_template = get_template('email/admin/contact/question.html')
+    email_context = Context({
+        'email': email,
+        'phone': phone,
+        'question_title': question_title,
+        'content': content,
+    })
+
+    # send email to admin
+    send_mail(
+        "[1:1 문의 답변] " + str(question_title),
+        email_template.render(email_context),
+        "예비맘 <contact@yebimom.com>",
+        ["contact@yebimom.com"],
+        fail_silently=False,
+    )
+
+
+def send_answer_email_to_user(email, question_title, content):
+    email_template = get_template('email/contact/answer.html')
+    email_context = Context({
+        'content': content
+    })
+
+    # send email to user
+    send_mail(
+        "[1:1 문의 답변] : " + str(question_title),
+        email_template.render(email_context),
+        "예비맘 <contact@yebimom.com>",
+        [email],
+        fail_silently=False,
+    )
+
+
+@shared_task
+def send_answer_email(email, phone, question_title, content):
+    send_answer_email_to_user(email, question_title, content)
+    send_answer_email_to_admin(email, phone, question_title, content)
