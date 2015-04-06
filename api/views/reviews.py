b6913663ef27ac2b69b5f9ec1f41508d9ef48617
dobestan <dobestan@gmail.com>
2015-04-06 16:09:14 +0900

    refactor api/views/reviews.py : only include modules currently using

--- api/views/reviews.py
+++ api/views/reviews.py
@@ -1,15 +1,12 @@
 from __future__ import absolute_import
 
 from rest_framework.generics import ListAPIView
-from rest_framework.response import Response
-
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from reviews.models import VisitReview as Review
 from reviews.models import UseReview
 
 from api.serializers.reviews import ReviewSerializer
-from centers.models.center import Center
 
 
 class UserReviewList(ListAPIView):
