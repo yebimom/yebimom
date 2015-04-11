bf5a37e267e0d4f59a415c549165289c132bcf3a
wikibootup <wikibootup@gmail.com>
2015-04-11 21:35:51 +0900

    update contact urls

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns(
     url(r'^login/$', 'users.views.login_view', name='login'),
     url(r'^logout/$', 'users.views.logout_view', name='logout'),
     url(r'^signup/$', 'users.views.signup', name='signup'),
+    url(r'^contact/$', 'users.views.contact', name='contact'),
 )
