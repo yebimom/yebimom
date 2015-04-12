cae5c77373dbcb99cdd889f9b55ffdf36c9c4053
wikibootup <wikibootup@gmail.com>
2015-04-13 02:16:21 +0900

    update settings : set i18n locale path

--- yebimom/yebimom/settings/partials/international.py
+++ yebimom/yebimom/settings/partials/international.py
@@ -1,5 +1,8 @@
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
+import os
+from yebimom.settings.partials.application import BASE_DIR
+
 
 LANGUAGE_CODE = 'ko-KR'
 
@@ -10,3 +13,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = False
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
