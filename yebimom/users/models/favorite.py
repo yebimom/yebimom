3b65235b57099c942c6583bbf8d365a038ee2a87
dobestan <dobestan@gmail.com>
2015-04-16 01:14:25 +0900

    init favorite model : has user, center information

---
+++ yebimom/users/models/favorite.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+from centers.models import Center
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User)
+    center = models.ForeignKey(Center)
+    created_at = models.DateTimeField(auto_now_add=True)
