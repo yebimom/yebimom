1cc07d0a9c5aea48b052a8520f4c047ce95fc0f5
dobestan <dobestan@gmail.com>
2015-04-16 01:15:33 +0900

    add condition : favorite object should have unique user_id, center_id

--- yebimom/users/models/favorite.py
+++ yebimom/users/models/favorite.py
@@ -8,3 +8,6 @@ class Favorite(models.Model):
     user = models.ForeignKey(User)
     center = models.ForeignKey(Center)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = (('user', 'center'),)
