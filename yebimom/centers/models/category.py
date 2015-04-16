3149da1b5cec74adcca2f75f85f2b08b977fa8ea
xme1226 <xme1226@gmail.com>
2015-04-17 00:38:36 +0900

    update category model : change slag to slug

--- yebimom/centers/models/category.py
+++ yebimom/centers/models/category.py
@@ -7,7 +7,7 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "Categories"
 
-    slag = models.CharField(max_length=20, unique=True, blank=True)
+    slug = models.CharField(max_length=20, unique=True, blank=True)
     center = models.ManyToManyField(Center, blank=True)
     name = models.CharField(max_length=40, unique=True)
 
