078c17e311772a408485df5edbe249fe2f354416
dobestan <dobestan@gmail.com>
2015-04-16 15:40:41 +0900

    toggle class on <-> off, when icon is clicked

--- yebimom/yebimom/static/js/yebimom.js
+++ yebimom/yebimom/static/js/yebimom.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
   $('.center i.favorite').click(function(){
     var center_hash_id = $(this).closest('.center').data('center_hash_id');
-    alert(center_hash_id);
+
+    if ($(this).hasClass('favorite-on')) {
+      $(this).removeClass('favorite-on');
+      $(this).addClass('favorite-off');
+    } else {
+      $(this).removeClass('favorite-off');
+      $(this).addClass('favorite-on');
+    }
   });
 });
