569fb40da467231e9087d05fbe91f8eb821f405e
xme1226 <xme1226@gmail.com>
2015-04-16 21:30:18 +0900

    refactor center views

--- yebimom/centers/views.py
+++ yebimom/centers/views.py
@@ -36,13 +36,21 @@ class CenterList(ListView):
     def get_queryset(self):
         search_query = self.request.GET.get('search') or str()
         location_query = self.request.GET.get('location') or str()
+
+        centers = Center.objects.filter(name__contains=search_query)
+
         if location_query is not '':
-            regions_second_layer = RegionSecondLayer.objects.filter(name__contains=location_query)
-            regions_third_layer = RegionThirdLayer.objects.filter(name__contains=location_query)
-            centers_region_second_layer = Center.objects.filter(region_second_layer__contains=regions_second_layer)
-            centers_region_third_layer = Center.objects.filter(region_third_layer__contains=regions_third_layer)
-            return centers_region_second_layer | centers_region_third_layer
-        return Center.objects.filter(name__contains=search_query)
+            centers = self.get_queryset_by_location(location_query)
+
+        return centers
+
+    def get_queryset_by_location(self, location_query):
+        regions_second_layer = RegionSecondLayer.objects.filter(name__contains=location_query)
+        regions_third_layer = RegionThirdLayer.objects.filter(name__contains=location_query)
+        centers_region_second_layer = Center.objects.filter(region_second_layer__contains=regions_second_layer)
+        centers_region_third_layer = Center.objects.filter(region_third_layer__contains=regions_third_layer)
+        return centers_region_second_layer | centers_region_third_layer
+
 
 
 class CenterDetail(DetailView):
