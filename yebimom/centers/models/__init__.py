13c939d5257ceb911c4fd8c4c8b0de01cd8cf79f
dobestan <dobestan@gmail.com>
2015-04-18 17:22:22 +0900

    add categories on main page

--- yebimom/centers/models/__init__.py
+++ yebimom/centers/models/__init__.py
@@ -4,3 +4,4 @@ from facility import Facility
 from policy import Policy
 from region import RegionFirstLayer, RegionSecondLayer, RegionThirdLayer
 from image import CenterImage
+from category import Category
