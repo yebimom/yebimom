8a527baf410930f53ef09e454d18e0ccee8fdaff
dobestan <dobestan@gmail.com>
2015-04-19 17:52:19 +0900

    update category model : add imagefield

--- yebimom/centers/models/category.py
+++ yebimom/centers/models/category.py
@@ -2,11 +2,17 @@ from django.db import models
 from centers.models.center import Center
 
 
+def _generate_upload_path(self, file_name):
+    return "category/%s/%s" % (self.slug, file_name)
+
+
 class Category(models.Model):
     centers = models.ManyToManyField(Center)
 
     name = models.CharField(max_length=40, unique=True)
     slug = models.CharField(max_length=20, unique=True, blank=True)
+
+    image = models.ImageField(upload_to=_generate_upload_path, blank=True, null=True)
 
     class Meta:
         verbose_name_plural = "Categories"
