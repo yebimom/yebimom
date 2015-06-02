10f408449030bc97c7fcef0dd1be64822190f224
dobestan <dobestan@gmail.com>
2015-06-02 21:21:34 +0900

    enable smooth scrolling on one-page movement

--- yebimom/yebimom/static/js/yebimom.js
+++ yebimom/yebimom/static/js/yebimom.js
@@ -1,3 +1,23 @@
+(function(){
+  // Smooth Scrolling
+  $('body').scrollspy({target: '#scrollspy'});
+
+  $(function() {
+    $('a[href*=#]:not([href=#]):not([data-toggle])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 500);
+          return false;
+        }
+      }
+    });
+  });
+})();
+
 $(document).ready(function(){
   var csrftoken = $.cookie('csrftoken');
 
