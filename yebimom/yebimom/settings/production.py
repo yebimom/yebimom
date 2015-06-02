a18b9773021209e71d5e0827c2e8135d2a3a05e2
dobestan <dobestan@gmail.com>
2015-06-02 19:35:03 +0900

    refactor settings : remove duplicate settings, and change settings location

--- yebimom/yebimom/settings/production.py
+++ yebimom/yebimom/settings/production.py
@@ -2,45 +2,28 @@ from yebimom.settings import *
 import os
 
 
-# partials/development.py
-
 DEBUG = False
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".yebimom.com", ]
 
 
-# partials/static.py
-
-# Serving static files from a cloud service or CDN
-# https://docs.djangoproject.com/en/1.7/howto/static-files/deployment/#serving-static-files-from-a-cloud-service-or-cdn
-STATIC_URL = 'https://cdn.yebimom.com/'
+INSTALLED_APPS = INSTALLED_APPS + (
+    'raven.contrib.django.raven_compat',
+)
 
 
 # https://docs.djangoproject.com/en/1.7/ref/settings/#default-file-storage
 # https://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html
 
+
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = 'yebimom.storage.S3PipelineManifestStorage'
+# STATICFILES_STORAGE = 'yebimom.storage.S3PipelineManifestStorage'
 
 STATIC_URL = 'https://cdn.yebimom.com/'
 
 
-# Setting for AWS
-
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-
-
-# sentry
-
-# Set DSN value
 RAVEN_CONFIG = {
     'dsn': os.environ['SENTRY_DSN'],
 }
 
-# Add raven to the list of installed apps
-INSTALLED_APPS = INSTALLED_APPS + (
-    'raven.contrib.django.raven_compat',
-)
