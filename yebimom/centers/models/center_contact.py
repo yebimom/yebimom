3b0ef752a1696610732967bf078fcaad10c98854
dobestan <dobestan@gmail.com>
2015-06-03 15:19:13 +0900

    update CenterContact : init post_save signal, should notify admin about model itself

--- yebimom/centers/models/center_contact.py
+++ yebimom/centers/models/center_contact.py
@@ -1,5 +1,7 @@
 from django.db import models
 from centers.models.center import Center
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class CenterContactManager(models.Manager):
@@ -14,3 +16,9 @@ class CenterContact(models.Model):
     def get_managers(self):
         return self.center.managers.all()
     managers = property(get_managers)
+
+
+@receiver(post_save, sender=CenterContact)
+def _create_center_contact(sender, instance, created, **kwargs):
+    if created:
+        pass
