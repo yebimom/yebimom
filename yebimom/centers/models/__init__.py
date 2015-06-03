5fd2bf843834390b03b196937a59a8cb38047adc
dobestan <dobestan@gmail.com>
2015-06-03 21:26:10 +0900

    init CenterLanding Model : Center has many CenterLanding objects

--- yebimom/centers/models/__init__.py
+++ yebimom/centers/models/__init__.py
@@ -6,3 +6,4 @@ from region import RegionFirstLayer, RegionSecondLayer, RegionThirdLayer
 from image import CenterImage
 from category import Category
 from center_contact import CenterContact
+from center_landing import CenterLanding
---
+++ yebimom/centers/models/center_landing.py
@@ -0,0 +1,23 @@
+from django.db import models
+from centers.models.center import Center
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from centers.utils.center_landing_hashids import get_encoded_center_landing_hashid
+
+
+class CenterLandingManager(models.Manager):
+    pass
+
+
+class CenterLanding(models.Manager):
+    objects = CenterLandingManager()
+
+    hash_id = models.CharField(max_length=12, unique=True, blank=True, null=True)
+    center = models.ForeignKey(Center)
+
+
+@receiver(post_save, sender=CenterLanding)
+def _create_center_landing(sender, instance, created, **kwargs):
+    if created:
+        instance.hash_id = get_encoded_center_landing_hashid(instance.id)
+        instance.save()
