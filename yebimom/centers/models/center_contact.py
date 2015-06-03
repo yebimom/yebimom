b60093dd07d416904b91a51204417685eb21e00c
dobestan <dobestan@gmail.com>
2015-06-03 15:12:39 +0900

    update CenterContact : add get_managers method, managers property to get all admins

--- yebimom/centers/models/center_contact.py
+++ yebimom/centers/models/center_contact.py
@@ -10,3 +10,7 @@ class CenterContact(models.Model):
     objects = CenterContactManager()
 
     center = models.ForeignKey(Center)
+
+    def get_managers(self):
+        return self.center.managers.all()
+    managers = property(get_managers)
