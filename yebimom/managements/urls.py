85f062d4b953702fbd4dbcfe5b3c30d27a7e135a
dobestan <dobestan@gmail.com>
2015-06-03 22:13:46 +0900

    update managements:landing-list : show landing lists related to parent center

--- yebimom/managements/urls.py
+++ yebimom/managements/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns(
     '',
 
     url(r'^$', ManagementDashboard.as_view(), name='dashboard'),
-    url(r'^center/(?P<slug>\w+)/landing/$', ManagementCenterLanding.as_view(), name='landing-list'),
+    url(r'^center/(?P<hash_id>\w+)/landing/$', ManagementCenterLanding.as_view(), name='landing-list'),
 )
--- yebimom/managements/views.py
+++ yebimom/managements/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import View, TemplateView
+from django.views.generic.list import ListView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from centers.models.center import Center
 
 
 class ManagementBaseView(View):
@@ -14,5 +16,11 @@ class ManagementDashboard(ManagementBaseView, TemplateView):
     template_name = "managements/home.html"
 
 
-class ManagementCenterLanding(ManagementBaseView, TemplateView):
+class ManagementCenterLanding(ManagementBaseView, ListView):
     template_name = "managements/landing.html"
+    context_object_name = "center_landings"
+
+    def get_queryset(self):
+        center = Center.objects.get(hash_id=self.kwargs['hash_id'])
+        return center.centerlanding_set.all()
+
