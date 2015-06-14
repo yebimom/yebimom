603965d7254653e62d3cc1f36d3353a00c9da71f
dobestan <dobestan@gmail.com>
2015-06-14 16:34:40 +0900

    update ManagementCenterLanding : ListView -> CreateView

--- yebimom/managements/views.py
+++ yebimom/managements/views.py
@@ -1,8 +1,11 @@
 from django.views.generic import View, TemplateView
-from django.views.generic.list import ListView
+# from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+
 from centers.models.center import Center
+from centers.models.center_landing import CenterLanding
 
 
 class ManagementBaseView(View):
@@ -16,15 +19,14 @@ class ManagementDashboard(ManagementBaseView, TemplateView):
     template_name = "managements/home.html"
 
 
-class ManagementCenterLanding(ManagementBaseView, ListView):
+class ManagementCenterLanding(ManagementBaseView, CreateView):
     template_name = "managements/landing.html"
-    context_object_name = "center_landings"
-
-    def get_queryset(self):
-        center = Center.objects.get(hash_id=self.kwargs['hash_id'])
-        return center.centerlanding_set.all()
+    model = CenterLanding
 
     def get_context_data(self, **kwargs):
         context = super(ManagementCenterLanding, self).get_context_data(**kwargs)
-        context['center'] = Center.objects.get(hash_id=self.kwargs['hash_id'])
+        center = Center.objects.get(hash_id=self.kwargs['hash_id'])
+        context['center'] = center
+        context['center_landings'] = center.centerlanding_set.all()
+
         return context
