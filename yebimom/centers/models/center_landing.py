09f075d0cf3cdd35d73b1969c69f57c50e29659f
dobestan <dobestan@gmail.com>
2015-06-18 13:30:48 +0900

    add feature : update CenterLanding with goo.gl shorten_url

--- yebimom/centers/models/center_landing.py
+++ yebimom/centers/models/center_landing.py
@@ -4,6 +4,7 @@ from centers.models.center import Center
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from centers.utils.center_landing_hashids import get_encoded_center_landing_hashid
+from api.tasks.shorten import url_builder, shorten_url
 
 from yebimom.settings import YEBIMOM_URL
 
@@ -20,6 +21,8 @@ class CenterLanding(models.Model):
 
     title = models.CharField(max_length=200, blank=True, null=True)
 
+    shorten_url = models.CharField(max_length=32, blank=True, null=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
@@ -33,4 +36,14 @@ class CenterLanding(models.Model):
 def _create_center_landing(sender, instance, created, **kwargs):
     if created:
         instance.hash_id = get_encoded_center_landing_hashid(instance.id)
+
+        instance.save()
+
+        # Create Shorten URL via goo.gl
+        full_url = instance.get_full_absolute_url()
+
+        instance_campaign_name = "landing-%s" % (instance.hash_id)
+        full_url_with_utm_params = url_builder(full_url, utm_source="yebimom", utm_medium="yebimom", utm_campaign=instance_campaign_name)
+        instance.shorten_url = shorten_url(full_url_with_utm_params)
+
         instance.save()
